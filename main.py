67305d4302cf98d6b65f5743df54e3dd66ba2c86
baokie <35675544+baokie@users.noreply.github.com>
2022-10-24 02:43:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-birthday2 = os.environ['BIRTHDAY2']
+birthday2 = os.environ['BIRTHDAYZ']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
